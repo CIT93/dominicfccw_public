e2226fe7e6dd7ea288f808b88f75c3711cf37fd3
0693163 <0693163@my.scccd.edu>
2021-01-21 13:37:31 -0800

    fixed errors

--- w2/stimulus.js
+++ w2/stimulus.js
@@ -1,21 +1,20 @@
 // $600 per adult
 // extra 600$ for children under 17
 // married couples eliglible for 1,200$ check
-let myStatus = 'Single'
-//let myStatus = 'Married'
-//let myStatus = 'headOfHouse'
 
-let grossIncome = 40000
-let dependentChildren = 2
-let stimulusPayment = 0
+let filingStatus = 'Single'
+let adjustedIncome = 74999
 
+let numberChildren = 1
+let checkAmount = 0
 
-if (myStatus = 'Single' && grossIncome <= 50000 ) {
-    stimulusPayment = 600 + dependentChildren * 600
-} else if (myStatus = 'Married' && grossIncome <= 100000) {
-    stimulusPayment = 1200 + dependentChildren * 600
-} else if (myStatus = 'headOfHouse' && grossIncome <= 90000) {
-    stimulusPayment = 600 + dependentChildren * 600
+// Check Logic
+if (filingStatus === 'Single' && adjustedIncome < 75000) {
+  checkAmount = 600 + numberChildren * 600
+} else if (filingStatus === 'Married' && adjustedIncome < 150000) {
+  checkAmount = 1200 + numberChildren * 600
+} else if (filingStatus === 'HOH' && adjustedIncome < 112500) {
+  checkAmount = 600 + numberChildren * 600
 }
 
-console.log('You are likely getting $' + stimulusPayment + ' of stimulus payment.')
+console.log('Your stimulus check will be $' + checkAmount + ' given your filing status of ' + filingStatus + ' and adjusted Income of ' + adjustedIncome)
